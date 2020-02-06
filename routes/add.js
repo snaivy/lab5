6e1f32f5a66d42213bbda3c23bf0ed0368f63382
Darwin Lee <darwinlee97@gmail.com>
2020-02-06 13:56:14 -0800

    completed lab 5

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,13 @@
 var data = require("../data.json");
 
 exports.addFriend = function(request, response) {    
-	// Your code goes here
+	var newFriend = {
+		"name": request.query.name,
+		"description": request.query.description,
+		//"imageURL": 'http://lorempixel.com/400/400/people'
+		"imageURL": 'https://vignette.wikia.nocookie.net/yuruyuri/images/8/80/AkariAkaza-YY10.png/revision/latest?cb=20191006144557'
+	};
+
+	data.friends.push(newFriend);
+	res.render('add', data);
  }
